ee889dbedbfcd5d95c2d5a00dc9b66aafb5aa13e
김채림 <cofla2525@naver.com>
2025-07-21 22:55:56 +0900

    작업자 : 김채림, 작업내용 : 웹 배포 라우팅 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  basePath: '/Project_MBTI',
+  assetPrefix: '/Project_MBTI',
 };
 
 module.exports = nextConfig;
